7c6f8d876d4c67dd0b99c94f53f64a3d2dd698f0
shaikhreza43 <reza.rk786@gmail.com>
2021-11-15 10:49:40 +0530

    implemented Search Movies,Shows using thunk

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,38 +1,64 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Footer from './Footer';
 import { movieApi } from '../utils/api';
 import { useDispatch, useSelector } from 'react-redux';
-import { loadMovies } from '../redux/movieSlice';
+import { loadMovies, loadMoviesAsync, loadShowsAsync, searchMovieAsync, searchShowAsync } from '../redux/movieSlice';
 
 export default function Home(props) {
+
+    const [searchText, setSearchText] = useState('');
+    const [showSearchText, setShowSearchText] = useState('');
 
     const dispatch = useDispatch();
 
     const movieList = useSelector(state => state.movies.movies);
+    const showList = useSelector(state => state.movies.shows);
 
     useEffect(() => {
+        //This is the Synchronous way of doing.
+        // const fetchMovieList = async () => {
+        //     const response = await movieApi.get("");
+        //     if (response.status === 200)
+        //         dispatch(loadMovies(response.data));
+        // }
 
-        const fetchMovieList = async () => {
-            const response = await movieApi.get("");
-            if (response.status === 200)
-                dispatch(loadMovies(response.data));
-        }
+        // fetchMovieList();
 
-        fetchMovieList();
+        //Doing Asynchronously using thunk
+        dispatch(loadMoviesAsync());
+        dispatch(loadShowsAsync());
     }, [dispatch]);
+
+    const handleMovieSearch = (e) => {
+        e.preventDefault();
+        if (searchText)
+            dispatch(searchMovieAsync({ searchText }));
+    }
+
+    const handleShowSearch = (e) => {
+        e.preventDefault();
+        if (showSearchText)
+            dispatch(searchShowAsync({ showSearchText }));
+    }
 
     return (
         <>
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-lg-12">
-                        <h2>Movies</h2>
+                        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                            <h2>Movies</h2>
+                            <form className="form-inline d-flex mt-3" onSubmit={handleMovieSearch}>
+                                <input className="form-control mr-sm-2" value={searchText} onChange={(e) => setSearchText(e.target.value)} type="search" placeholder="Search Movies" aria-label="Search" />
+                                <button className="btn btn-outline-success mx-2" type="submit">Search</button>
+                            </form>
+                        </div>
                         <div className="movie-list-outer-div">
 
                             {movieList.Response === "True" ?
                                 movieList.Search.map((movie, index) => (
 
-                                    <div key={index} className="card" style={{ width: '15rem', height: 'auto', margin: '20px 40px' }}>
+                                    <div key={index} className="card" style={{ width: '10rem', height: 'auto', margin: '10px 20px' }}>
                                         <img className="card-img-top" src={movie.Poster} alt={movie.Title} />
                                         <div className="card-body">
                                             <p className="card-text">{movie.Title}</p>
@@ -42,6 +68,31 @@ export default function Home(props) {
                                 )) :
                                 (<div><h3>{movieList.Error}</h3></div>)}
                         </div>
+                    </div>
+                </div>
+
+                <div className="row mt-3">
+                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                        <h2>TV Shows</h2>
+                        <form className="form-inline d-flex mt-3" onSubmit={handleShowSearch}>
+                            <input className="form-control mr-sm-2" value={showSearchText} onChange={(e) => setShowSearchText(e.target.value)} type="search" placeholder="Search Shows" aria-label="Search" />
+                            <button className="btn btn-outline-success mx-2" type="submit">Search</button>
+                        </form>
+                    </div>
+                    <div className="show-list-outer-div">
+
+                        {showList.Response === "True" ?
+                            showList.Search.map((show, index) => (
+
+                                <div key={index} className="card" style={{ width: '10rem', height: 'auto', margin: '10px 20px' }}>
+                                    <img className="card-img-top" src={show.Poster} alt={show.Title} />
+                                    <div className="card-body">
+                                        <p className="card-text">{show.Title}</p>
+                                    </div>
+                                </div>
+
+                            )) :
+                            (<div><h3>{showList.Error}</h3></div>)}
                     </div>
                 </div>
 
--- src/redux/movieSlice.js
+++ src/redux/movieSlice.js
@@ -1,11 +1,81 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { movieApi } from '../utils/api';
+import axios from 'axios';
+
+export const loadMoviesAsync = createAsyncThunk("movies/loadMoviesAsync", async () => {
+    const response = await movieApi.get("");
+    if (response.status === 200) {
+        return response.data;
+    }
+});
+
+export const loadShowsAsync = createAsyncThunk("shows/loadShowsAsync", async () => {
+    const response = await axios.get("https://www.omdbapi.com/?i=tt3896198&apikey=7db14e37&s=friends&type=series");
+    if (response.status === 200) {
+        return response.data;
+    }
+});
+
+export const searchMovieAsync = createAsyncThunk("movies/searchMovieAsync", async (payload) => {
+    const response = await axios.get("https://www.omdbapi.com/?i=tt3896198&apikey=7db14e37&s=" + payload.searchText + "&type=movie");
+    if (response.status === 200)
+        return response.data;
+});
+
+export const searchShowAsync = createAsyncThunk("shows/searchShowAsync", async (payload) => {
+    const response = await axios.get("https://www.omdbapi.com/?i=tt3896198&apikey=7db14e37&s=" + payload.showSearchText + "&type=series");
+    if (response.status === 200)
+        return response.data;
+});
 
 const movieSlice = createSlice({
     name: "movies",
-    initialState: { movies: {} },
+    initialState: { movies: {}, shows: {} },
     reducers: {
         loadMovies: (state, action) => {
             state.movies = action.payload;
+        }
+    },
+    extraReducers: {
+        [loadMoviesAsync.pending]: () => {
+            console.log("Fetching Movies...");
+        },
+        [loadMoviesAsync.fulfilled]: (state, action) => {
+            console.log("Fetched Movies Successfully.");
+            state.movies = action.payload;
+        },
+        [loadMoviesAsync.rejected]: () => {
+            console.log("Request Got Rejected...");
+        },
+        [loadShowsAsync.pending]: () => {
+            console.log("Fetching Shows...");
+        },
+        [loadShowsAsync.fulfilled]: (state, action) => {
+            console.log("Fetched Shows Successfully.");
+            state.shows = action.payload;
+        },
+        [loadShowsAsync.rejected]: () => {
+            console.log("Request Got Rejected...");
+        },
+        [searchMovieAsync.pending]: () => {
+            console.log("Searching Movie...");
+        },
+        [searchMovieAsync.fulfilled]: (state, action) => {
+            console.log("Movie Search Completed...");
+            state.movies = action.payload;
+        },
+        [searchMovieAsync.rejected]: () => {
+            console.log("Movie Search Rejected...");
+        },
+        [searchShowAsync.pending]: () => {
+            console.log("Searching Show...");
+        },
+        [searchShowAsync.fulfilled]: (state, action) => {
+            console.log("Show Search Completed...");
+            state.shows = action.payload;
+        },
+        [searchShowAsync.rejected]: () => {
+            console.log("Show Search Rejected...");
         }
     }
 });
